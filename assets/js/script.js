fcbad4e5a0da222923075285916964aec22d689a
Nicholas Easter <nicholas.easter17@gmail.com>
2021-12-24 01:07:41 -0800

    got api information from cataas api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-var catSubmitEl = document.querySelector('#cat-button');
+const inputEl = document.querySelector('#cat-input');
+const catEl = document.querySelector('#cat-picture');
+const formEl = document.querySelector('#form');
+
+const gifEl = document.querySelector('#gif');
+const saysEl = document.querySelector('#says');
+const colorEl = document.querySelector('#color');
+
+formEl.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var url = "https://cataas.com/cat";
+
+    if(gifEl.checked) {
+        console.log("GIF");
+        url += "/gif";
+    }if(saysEl.value) {
+        console.log("saysEl");
+        url += "/says/" + saysEl.value;
+        
+    }if(colorEl.value) {
+        console.log("colorEl");
+        url += "?color=" + colorEl.value;
+    }
+
+    catEl.src = url;
+})
